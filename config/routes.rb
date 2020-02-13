5ab7499b30bb2cbabc4b283affd3890e92b1c1f2
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-02-13 10:40:41 +0900

    ログイン・ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  #ユーザー管理機能のルーティング
+  resources :users, only: [:new, :create, :show]
+
+  #セッション管理機能のルーティング
+  resources :sessions, only: [:new, :create, :destroy]
+
+  #写真投稿機能のルーティング
   resources :feeds do
     collection do
       post :confirm
     end
   end
-
-  resources :users, only: [:new, :create]
 end
